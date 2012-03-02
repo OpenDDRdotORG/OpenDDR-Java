91dc8667e99223f138ad689b7c0ea36dfb6b3e31
Rob Rohan <rob@virtualtourist.com>
2012-03-02 11:11:32 -0800

    Adding in copyright and comments

--- src/main/java/org/openddr/simpleapi/oddr/FileLocator.java
+++ src/main/java/org/openddr/simpleapi/oddr/FileLocator.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2011 OpenDDR LLC
+ * This software is distributed under the terms of the GNU Lesser General Public License.
+ *
+ *
+ * This file is part of OpenDDR Simple APIs.
+ * OpenDDR Simple APIs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * OpenDDR Simple APIs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Simple APIs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.openddr.simpleapi.oddr;
 
 import java.io.File;
@@ -6,9 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Copyright 2012 VirtualTourist.com
- * Date: 3/2/12 10:43 AM
- * Created By: rob
+ * Created By: rob @ VirtualTourist.com 2012-03-02 10:43AM
  */
 public class FileLocator {
 
