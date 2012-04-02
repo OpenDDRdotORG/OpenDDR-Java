20342c546a00ec8640753aa19f40b1600ce24686
Matt Sponer <matt@churnlabs.com>
2012-04-02 09:06:09 -0700

    Build script cleans up temporary file

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ mkdir build
 find ./src -name "*.java" > sources_list.txt
 javac -classpath `find lib -name '*.jar' | xargs echo | tr ' ' ':'` @sources_list.txt -d build
 jar -cf OpenDDR.jar -C build/ .
+rm -f sources_list.txt
