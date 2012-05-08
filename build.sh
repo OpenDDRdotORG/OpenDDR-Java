887ee57847e44a8eb39a38732d75f2e683c1cfd1
Matt Sponer <matt@churnlabs.com>
2012-05-08 11:35:32 -0700

    Remove build script so we are closer to OpenDDRdotORG/OpenDDR-Java

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Quick and dirty build shell script
-mkdir build
-find ./src -name "*.java" > sources_list.txt
-javac -classpath `find lib -name '*.jar' | xargs echo | tr ' ' ':'` @sources_list.txt -d build
-jar -cf OpenDDR.jar -C build/ .
-rm -f sources_list.txt
